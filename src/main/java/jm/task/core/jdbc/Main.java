51449da1d19fa3ce52dec0e61f091e75db93d103
Zharklod <d.zharckov2013@yandex.ru>
2024-10-23 15:24:01 +0300

    JDBC ver. 2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,21 +1,23 @@
 package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userServiceImpl = new UserServiceImpl();
-        userServiceImpl.createUsersTable();
-        userServiceImpl.saveUser("Андрей", "Губин", (byte) 52);
-        userServiceImpl.saveUser("Антон", "Камолов", (byte) 42);
-        userServiceImpl.saveUser("Гарик", "Харламов", (byte) 45);
-        userServiceImpl.saveUser("Сергей", "Жуков", (byte) 50);
-        userServiceImpl.getAllUsers();
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Андрей", "Губин", (byte) 52);
+        userService.saveUser("Антон", "Камолов", (byte) 42);
+        userService.saveUser("Гарик", "Харламов", (byte) 45);
+        userService.saveUser("Сергей", "Жуков", (byte) 50);
+        userService.getAllUsers();
 //        for (int i = 4; i > 0; i--) {
 //            userDaoJDBCImpl.removeUserById(i);
 //        }
-        userServiceImpl.cleanUsersTable();
-        userServiceImpl.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
         Util.closeConnection();
     }
 }
